59b3e34eb27e16597f745cb6bb253a4ca6002113
Ron <turetzkyron@gmail.com>
2022-03-08 17:58:06 +0200

    Adding fields removing age

--- server/models/User.js
+++ server/models/User.js
@@ -4,11 +4,13 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
 	name: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
-	age: {type: Number, required: true, unique:true},
 	gender: {type: String, required: true, unique: true},
 	attract_to: {type: String, required: true, unique: false},
 	password: { type: String, required: true },
 	matches: [{ type: mongoose.Schema.Types.ObjectId, ref: "Match" }],
+	relevant:[{type:mongoose.Schema.Types.ObjectId,ref:"person"}],
+	swiped_yes:[{type:mongoose.Schema.Types.ObjectId,ref:"matched_person"}]
+
 });
 
 module.exports = mongoose.model("User", userSchema);

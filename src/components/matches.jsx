ec23719d79835c302251217e6844b341b981ce5c
Abziz <Abziz89@gmail.com>
2022-03-09 21:59:39 +0200

    improve frontend

--- src/components/matches.jsx
+++ src/components/matches.jsx
@@ -1,82 +1,65 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import { AuthContext } from "../contexts/auth";
-import AddMatch from "./addMatch";
 import meetTelHaiService from "../services/meetTelHaiService";
 
 class Matches extends Component {
-  static contextType = AuthContext;
-  state = {
-    match: null,
-  };
-  async componentDidMount() {
-    try {
-      const { data } = await meetTelHaiService.allMatches();
-      this.setState({ match: data });
-    } catch (err) {
-      console.error(err);
-    }
-  }
-  setStateOfMatches = () => {
-    console.log("remounting");
-    this.componentDidMount();
-  };
+	static contextType = AuthContext;
+	state = {
+		matches: null,
+		error: null
+	};
+	async componentDidMount() {
+		try {
+			const { data } = await meetTelHaiService.allMatches();
+			this.setState({ matches: data });
+		} catch (err) {
+			this.setState(err);
+		}
+	}
+	setStateOfMatches = () => {
+		console.log("remounting");
+		this.componentDidMount();
+	};
 
-  render() {
-    if (this.state.match === null) {
-      return <p>loading...</p>;
-    }
+	render() {
+		if (this.state.matches === null) {
+			return <p>loading...</p>;
+		}
+		if (this.state.error) {
+			return <p> Sorry, something went wrong</p>
+		}
 
-    if (this.state.match.length === 0) {
-      return (
-        <div className="row h-100 bg-danger">
-        <div className="container">
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <h4 className="text-center">You still don't have any match
-          <br></br>
-           Click on "Meet" to make some</h4>
-        </div>
-        </div>
-      );
-    } else {
-      // there are matches
-      return (
-        <div className="row h-100 bg-danger">
-        <div className="container">
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <h4 className="text-center">You still don't have any match
-          <br></br>
-           Click on "Meet" to make some</h4>
-        </div>
-        </div>
-      //   <div className="container">
-      //   <h4 className="text-center">Choose the match you would like to contact with</h4>
-      //   {this.state.matches.map((match, i) => (
-      //     <div className="card p-2 m-2 border-light border-rounded">
-      //           <Label as='a' image>
-      //             {match.name}
-      //           </Label>
-      //           <div>
-      //             <Button theme="pink" onClick={clickMe}>
-      //               Show Number
-      //             </Button>
-      //           </div>
-      //       <pre></pre>
-      //     </div>
-      //   ))}
-      // </div>
-        
-      );
-    }
-  }
+		if (this.state.matches.length === 0) {
+			return (
+				<div className="row h-100 bg-danger">
+					<div className="container">
+						<h4 className="text-center">You still don't have any match
+							<br></br>
+							Click on "Meet" to make some</h4>
+					</div>
+				</div>
+			);
+		}
+
+		return (
+			<div className="container">
+				<h4 className="text-center">Choose the match you would like to contact with</h4>
+				{this.state.matches.map((match, i) => (
+					<div className="card p-2 m-2 border-light border-rounded">
+						<label as='a' image>
+							{match.name}
+						</label>
+						<div>
+							<button theme="pink" onClick={console.log}>
+								Show Number
+							</button>
+						</div>
+						<pre></pre>
+					</div>))
+				}
+			</div>
+
+		);
+	}
 }
-
 export default Matches;
--- src/components/meet.jsx
+++ src/components/meet.jsx
@@ -1,31 +1,75 @@
 import React, { Component } from "react";
-import Card from "react-ui-cards";
-import { Button} from 'react-bootstrap';
-import Datingcards from "../Datingcards/Datingcards";
+
+import meetTelHaiService from "../services/meetTelHaiService";
 // import Datingcards from "../Datingcards/Datingcards"
-// import { AuthContext } from "./../contexts/auth";
+import { AuthContext } from "./../contexts/auth";
+import TinderCard from "react-tinder-card";
 
 class Meet extends Component {
-    state = {};
-    render(){
-        return(
-            
-            <Datingcards/>
-            // <div className="meet">
-            //     <Card style={{ width: '18rem' }}>
-            //     <Card.Img variant="top" src="/homepagepic.jpg" />
-            //     <Card.Body>
-            //     <Card.Title>Card Title</Card.Title>
-            //     <Card.Text>
-            //     Some quick example text to build on the card title and make up the bulk of
-            //     the card's content.
-            //     </Card.Text>
-            //     <Button variant="primary">Go somewhere</Button>
-            //     </Card.Body>
-            //     </Card>
-            // </div>
-        );
-    }
+	static contextType = AuthContext;
+	state = {
+		users: null,
+		error: null
+	};
+	async componentDidMount() {
+		const { data } = await meetTelHaiService.available();
+		this.setState({ users: data });
+	}
+	getUserImage(user) {
+
+		if (user.image) {
+			return user.image
+		}
+		return `https://robohash.org/${user._id}`
+
+	}
+	onSwipe = async (direction, user) => {
+		console.log({ direction, user })
+		await meetTelHaiService.like(direction === "right", user._id)
+	}
+	render() {
+		if (this.state.users == null) {
+			return <div>Loading...</div>
+		}
+		if (this.state.error) {
+			return (<>
+				<div> Sorry something is wrong </div>
+				<div> {this.state.error}</div>
+			</>);
+		}
+
+		return (
+			<div>
+				<div className='cardContainer'>
+					{this.state.users.map((user) =>
+						<TinderCard className='swipe' key={user.name} onSwipe={(dir) => this.onSwipe(dir, user)}>
+							<div style={{ backgroundImage: 'url(' + this.getUserImage(user) + ')' }} className='card'>
+								<h3>{user.name}</h3>
+							</div>
+						</TinderCard>
+					)}
+				</div>
+
+
+				{/* <div className="position-relative ">
+					{
+						this.state.users.map(user => (<div className="position-absolute top-0">
+							<div className="card" style={{ width: "18rem" }}>
+								<img className="card-img-top" src={this.getUserImage(user)} alt="hi" />
+								<div className="card-body">
+									<div></div>
+									<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								</div>
+								<pre>{JSON.stringify(user, null, 2)}</pre>
+							</div>
+						</div>)
+						)
+					}
+				</div> */}
+			</div >
+			// <Datingcards />
+		);
+	}
 }
 
 export default Meet;
